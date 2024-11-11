331de1629d7416d1a47163fcf1004f5b0dfae3db
Hiroki Kozuki <110721532+hiroki-kozuki@users.noreply.github.com>
2024-11-11 14:44:17 +0000

    Add files via upload

---
+++ build/CMakeFiles/config3NewWire5LayerMWPC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/config3NewWire5LayerMWPC.dir/config3NewWire5LayerMWPC.C.o"
+  "CMakeFiles/config3NewWire5LayerMWPC.dir/config3NewWire5LayerMWPC.C.o.d"
+  "config3NewWire5LayerMWPC"
+  "config3NewWire5LayerMWPC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/config3NewWire5LayerMWPC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/config3NewWire5LayerMWPC.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for config3NewWire5LayerMWPC.
+# This may be replaced when dependencies are built.
