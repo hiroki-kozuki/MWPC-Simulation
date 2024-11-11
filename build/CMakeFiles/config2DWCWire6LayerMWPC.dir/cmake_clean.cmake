4aa2c2727b7d4cb9b59f99f45a4e52e54daa1f00
Hiroki Kozuki <110721532+hiroki-kozuki@users.noreply.github.com>
2024-11-11 14:42:50 +0000

    Add files via upload

---
+++ build/CMakeFiles/config2DWCWire6LayerMWPC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/config2DWCWire6LayerMWPC.dir/config2DWCWire6LayerMWPC.C.o"
+  "CMakeFiles/config2DWCWire6LayerMWPC.dir/config2DWCWire6LayerMWPC.C.o.d"
+  "config2DWCWire6LayerMWPC"
+  "config2DWCWire6LayerMWPC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/config2DWCWire6LayerMWPC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/config2DWCWire6LayerMWPC.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for config2DWCWire6LayerMWPC.
+# This may be replaced when dependencies are built.
