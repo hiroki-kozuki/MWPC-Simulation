523aafa8b3ad8fef9c5d3227962a0f58635fe853
Hiroki Kozuki <110721532+hiroki-kozuki@users.noreply.github.com>
2024-11-11 14:29:15 +0000

    Add files via upload

---
+++ CMakeFiles/CMakeTemp/CMakeFiles/cmTC_a7324.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_a7324.dir/OpenMPTryFlag.c.o"
+  "cmTC_a7324"
+  "cmTC_a7324.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_a7324.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/CMakeTemp/CMakeFiles/cmTC_a7324.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_a7324.
+# This may be replaced when dependencies are built.
