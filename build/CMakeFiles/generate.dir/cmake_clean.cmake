d098df3b7d6ff2c8dd16e912c51bd84ab27131e4
Hiroki Kozuki <110721532+hiroki-kozuki@users.noreply.github.com>
2024-11-11 14:48:59 +0000

    Add files via upload

---
+++ build/CMakeFiles/generate.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/generate.dir/generate.C.o"
+  "CMakeFiles/generate.dir/generate.C.o.d"
+  "generate"
+  "generate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/generate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
