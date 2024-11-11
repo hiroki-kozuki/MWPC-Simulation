c3f21aec27ab9abfa759373ff452969f9335dafa
Hiroki Kozuki <110721532+hiroki-kozuki@users.noreply.github.com>
2024-11-11 14:41:21 +0000

    Add files via upload

---
+++ build/CMakeFiles/config1OldFoil5LayerMWPC.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/afs/cern.ch/work/k/kozuki/git/garfield/Examples/neBEM/config1OldFoil5LayerMWPC.C" "CMakeFiles/config1OldFoil5LayerMWPC.dir/config1OldFoil5LayerMWPC.C.o" "gcc" "CMakeFiles/config1OldFoil5LayerMWPC.dir/config1OldFoil5LayerMWPC.C.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/config1OldFoil5LayerMWPC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/config1OldFoil5LayerMWPC.dir/config1OldFoil5LayerMWPC.C.o"
+  "CMakeFiles/config1OldFoil5LayerMWPC.dir/config1OldFoil5LayerMWPC.C.o.d"
+  "config1OldFoil5LayerMWPC"
+  "config1OldFoil5LayerMWPC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/config1OldFoil5LayerMWPC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
