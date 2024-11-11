f717125996d70340c5a4e84f608065569f041415
Hiroki Kozuki <110721532+hiroki-kozuki@users.noreply.github.com>
2024-11-11 14:46:02 +0000

    Add files via upload

---
+++ build/CMakeFiles/config4Foil6LayerMWPC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/config4Foil6LayerMWPC.dir/config4Foil6LayerMWPC.C.o"
+  "CMakeFiles/config4Foil6LayerMWPC.dir/config4Foil6LayerMWPC.C.o.d"
+  "config4Foil6LayerMWPC"
+  "config4Foil6LayerMWPC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/config4Foil6LayerMWPC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/config4Foil6LayerMWPC.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for config4Foil6LayerMWPC.
+# This may be replaced when dependencies are built.
